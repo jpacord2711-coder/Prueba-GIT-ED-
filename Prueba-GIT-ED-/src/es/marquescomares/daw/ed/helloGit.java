302f232b94971062790ed2088ab0d45c44b5aec7
diurno <dgartej2207@iesmarquesdecomares.com>
2025-10-27 13:23:50 +0100

    feat(helloGit) third commit by Dario Garcia

--- Prueba-GIT-ED-/src/es/marquescomares/daw/ed/helloGit.java
+++ Prueba-GIT-ED-/src/es/marquescomares/daw/ed/helloGit.java
@@ -5,6 +5,8 @@ public class helloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello git, Juan Pacheco Ordóñez");
 		System.out.println("Hello git, Samuel Pineda Egea");
+		System.out.println("Hello git, Dario Garcia Tejero");
+
 
 	}
 
