d03e53a218f07641889e2adf6479f7df3b1ccf29
Tu Nombre <spinege0501@iesmarquesdecomares.org>
2025-10-27 13:19:58 +0100

    feat(helloGit) second commit by Samuel Pineda

--- Prueba-GIT-ED-/src/es/marquescomares/daw/ed/helloGit.java
+++ Prueba-GIT-ED-/src/es/marquescomares/daw/ed/helloGit.java
@@ -4,6 +4,7 @@ public class helloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello git, Juan Pacheco Ordóñez");
+		System.out.println("Hello git, Samuel Pineda Egea");
 
 	}
 
